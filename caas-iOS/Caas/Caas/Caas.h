1d22b5e81a95a70286796d929da1c549963c090a
Songtao <sontao.ma@circletable.com>
2017-01-02 14:19:22 +0800

    Create iOS framework project

---
+++ caas-iOS/Caas/Caas/Caas.h
@@ -0,0 +1,19 @@
+//
+//  Caas.h
+//  Caas
+//
+//  Created by Ma SongTao on 1/2/17.
+//  Copyright © 2017 shmakergroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Caas.
+FOUNDATION_EXPORT double CaasVersionNumber;
+
+//! Project version string for Caas.
+FOUNDATION_EXPORT const unsigned char CaasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Caas/PublicHeader.h>
+
+
