eb6924d64c0ba7e8b7a2f9dd2793db6bf5d0dc10
Songtao <sontao.ma@circletable.com>
2017-01-03 17:46:15 +0800

    支持Cocoapod

---
+++ caas-iOS/Caas/Classes/CaasGet.h
@@ -0,0 +1,13 @@
+//
+//  CaasGet.h
+//  Caas
+//
+//  Created by Ma SongTao on 1/3/17.
+//  Copyright © 2017 shmakergroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CaasGet : NSObject
+
+@end
---
+++ caas-iOS/Caas/Classes/CaasGet.m
@@ -0,0 +1,13 @@
+//
+//  CaasGet.m
+//  Caas
+//
+//  Created by Ma SongTao on 1/3/17.
+//  Copyright © 2017 shmakergroup. All rights reserved.
+//
+
+#import "CaasGet.h"
+
+@implementation CaasGet
+
+@end
